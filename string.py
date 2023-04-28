11989e240a1bac407d8c1d6898414130acd36ae7
TRENWAR <69980010+TRENWAR@users.noreply.github.com>
2023-04-28 15:39:24 +0700

    cringe
    this is my first kinda big project in python when i was in 7th grade, i did some change becuz why not i have nothing else better to do with my life haha...

--- string.py
+++ string.py
@@ -1,124 +1,105 @@
-# import module
 import os
-from time import sleep
+import time
 
 
-# it's just color
-white  = '\033[37m'
-cyan   = '\033[36m'
-yellow = '\033[93m'
-green  = '\033[92m'
-pink   = '\033[95m'
-red    = '\033[91m'
+# COLOR
+WHITE = "\033[37m"
+CYAN = "\033[36m"
+YELLOW = "\033[93m"
+GREEN = "\033[92m"
+PINK = "\033[95m"
+RED = "\033[91m"
 
 
-
-# def lenInPython()
-def lenInPython():
+def len_in_python():
     print("The len() function returns the number of items in an object")
-    sleep(0.5)
-    print("When the object is a string, the len() function returns the number of characters in the string")
+    time.sleep(0.5)
+    print(
+        "When the object is a string, the len() function returns the number of characters in the string"
+    )
     user = input("Want to try it Y/n: ")
-    if user == "Y":
+    if user in ["y", "yes", "Y"]:
         print("Type something")
         user = input("~~> ")
         calculate_typed_users = len(user)
-        sleep(0.4)
+        time.sleep(0.4)
         print(calculate_typed_users)
         print(f"the characters you type are {calculate_typed_users} characters")
-        sleep(0.4)
+        time.sleep(0.4)
         print("spaces are also counted")
-        
-    elif user == "n":
+
+    else:
         print("oke")
-        sleep(0.6)
-        mainMenu()
-        
-        
-        
-        
-        
-# def mainMenu
-def mainMenu():
+        time.sleep(0.6)
+        main_menu()
+
+
+def main_menu():
     os.system("clear")
-    sleep(0.3)
-    print(10*"="+"Welcome"+10*"=")
+    time.sleep(0.3)
+    print(10 * "=" + "Welcome" + 10 * "=")
     print("Welcome to this tools\r\nThis tools can manipulate string (using Python)")
     print("1.Use len()")
-    user = input("~~> ")
-    if user == "1":
-        lenInPython()
+    user_input = input("~~> ")
+    if user_input == "1":
+        len_in_python()
     else:
-        os.system("exit")
-    
-    
-    
-    
-    
+        pass
 
-    
-    
-    
-# def chooseColor
-def chooseColor():
+
+def choose_color():
     os.system("clear")
-    print(10*"="+"Welcome"+10*"=")
+    print(10 * "=" + "Welcome" + 10 * "=")
     print("Before we start, please select the color first")
-    print("1.Original (white (cuz you're original & simple boy)\r\n2.Red (cuz you like Red..??)\r\n3.Yellow (cuz idk why)\r\n4.Pink (cuz u watched Barbie or something)\r\n5.Cyan (cuz that a nice color???)\r\n6.Green (cuz u want to look like Hacker (fck)")
-    user = input("~~> ")
-    
-    
-    if user == "1":
-        print(white+"Nice, this is what i've been waiting for 20 years...Nice :)")
-        sleep(0.8)
-        mainMenu()
-        
-        
-    elif user == "2":
-        print(red+"Nice, i mean pretty nice :)")
-        sleep(0.9)
-        mainMenu()
-        
-        
-    elif user == "3":
-        print(yellow+"Oke")
-        sleep(0.4)
-        mainMenu()
-        
-        
-        
-    elif user == "4":
-        print(pink+"Nice fcking choise :D")
-        sleep(0.8)
-        mainMenu()
-        
-        
-    elif user == "5":
-        print(cyan+"Cool mode on i mean Cyan")
-        sleep(0.6)
-        mainMenu()
-        
-        
-    elif user == "6":
-        print(green+"if u like green & u choose this Np\r\nbut if u wan't to look like Hacker this is not hacking tools Wtf\r\nbut Np u can use this tools Xd")
-        sleep(0.4)
-        
-        user = input("So do u wan't to use my tool Y/n: ")
-        if user == "Y":
+    print(
+        "1.Original (white (cuz you're original & simple boy)\r\n2.Red (cuz you like Red..??)\r\n3.Yellow (cuz idk why)\r\n4.Pink (cuz u watched Barbie or something)\r\n5.Cyan (cuz that a nice color???)\r\n6.Green (cuz u want to look like Hacker (fck)"
+    )
+    user_input = input("~~> ")
+
+    if user_input == "1":
+        print(WHITE + "Nice, this is what i've been waiting for 20 years...Nice :)")
+        time.sleep(0.8)
+        main_menu()
+
+    elif user_input == "2":
+        print(RED + "Nice, i mean pretty nice :)")
+        time.sleep(0.9)
+        main_menu()
+
+    elif user_input == "3":
+        print(YELLOW + "Oke")
+        time.sleep(0.4)
+        main_menu()
+
+    elif user_input == "4":
+        print(PINK + "Nice fcking choise :D")
+        time.sleep(0.8)
+        main_menu()
+
+    elif user_input == "5":
+        print(CYAN + "Cool mode on i mean Cyan")
+        time.sleep(0.6)
+        main_menu()
+
+    elif user_input == "6":
+        print(
+            GREEN
+            + "if u like green & u choose this Np\r\nbut if u wan't to look like Hacker this is not hacking tools Wtf\r\nbut Np u can use this tools Xd"
+        )
+        time.sleep(0.4)
+
+        user_input = input("So do u wan't to use my tool Y/n: ")
+        if user_input == "Y":
             print("Oke")
-            sleep(0.4)
+            time.sleep(0.4)
         else:
             print("Oke")
-            os.system("exit")
-    
+
     else:
         print("Im serious")
-        
-        
-        
+        time.sleep(0.5)
+        choose_color()
 
 
-
-
-
-chooseColor()
+if __name__ == "__main__":
+    choose_color()
